4be2be8b1446516f4b7c251509c24e40a6760eed
cara <cara.lynch@wsp.com>
2021-02-05 14:31:31 +0000

    started adding rounding and main

--- local_freight_tool/zone_correspondence.py
+++ local_freight_tool/zone_correspondence.py
@@ -531,7 +531,36 @@ def point_zone_handling(
     return new_zone_corr, point_zones_info
 
 def round_zone_correspondence(zone_corr_no_slithers, zone_names):
-    
+    # find number of zone 2 zones that each zone 1 zone divides into
+    gbfm_counts = zone_corr_no_slithers.groupby(f"{zone_names[0]}_zone_id").size()
+
+    # create rounded zone correspondence
+    zone_corr_rounded = zone_corr_no_slithers[[f"{zone_names[0]}_zone_id", f"{zone_names[1]}_zone_id", f"{zone_names[0]}_to_{zone_names[1]}"]]
+
+    # for zone 1 zones that only map to one zone 2 zone, set adjustment factor to 1
+    zone_corr_rounded.loc[zone_corr_rounded[f"{zone_names[0]}_zone_id"].isin(gbfm_counts[gbfm_counts == 1].index), f"{zone_names[0]}_to_{zone_names[1]}"] = 1.0
+ 
+    # calculate missing zone 1 to zone 2 adjustments for those that don't have a one to one mapping
+    rest_to_round = zone_corr_rounded[zone_corr_rounded[f"{zone_names[0]}_zone_id"].isin(gbfm_counts[gbfm_counts > 1].index)]
+    differences = (1 - rest_to_round[[f"{zone_names[0]}_zone_id", f"{zone_names[0]}_to_{zone_names[1]}"]].groupby(f"{zone_names[0]}_zone_id").sum())
+
+    # add counts to differences
+    differences['counts'] = gbfm_counts[gbfm_counts > 1].astype(int)
+
+    # calculate portion of adjustment factor to add to each noham zone
+    differences['correction'] = differences[f"{zone_names[0]}_to_{zone_names[1]}"] / differences['counts']
+
+    # add correction to adjustment factor
+    rest_to_round = rest_to_round.merge(differences['correction'], how='inner', left_on=f"{zone_names[0]}_zone_id", right_on=differences.index)
+
+    rest_to_round[f"{zone_names[0]}_to_{zone_names[1]}"] = rest_to_round[f"{zone_names[0]}_to_{zone_names[1]}"] + rest_to_round['correction']
+
+    zone_corr_rounded.loc[zone_corr_rounded[f"{zone_names[0]}_zone_id"].isin(rest_to_round[f"{zone_names[0]}_zone_id"]), f"{zone_names[0]}_to_{zone_names[1]}"] = rest_to_round[f"{zone_names[0]}_to_{zone_names[1]}"]
+
+    return zone_corr_rounded
+
+    # TODO fix NaNs appearing in rounded zone corr, e.g. for gbfm zone 31274
+
 
 def main_zone_correspondence(
     zone_1_path,
@@ -590,6 +619,8 @@ def main_zone_correspondence(
         
         else:
             new_zone_corr = spatial_correspondence_no_slithers[[f"{zone_names[0]}_zone_id", f"{zone_names[1]}_zone_id", f"{zone_names[0]}_to_{zone_names[1]}"]]
+
+    # TODO finish this main function
         
 
 
