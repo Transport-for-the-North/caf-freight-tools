d184dd13bf4ba451fbf9472018d4eee0af46cdfc
clynch-wsp <cara.lynch@wsp.com>
2021-02-16 14:52:23 +0000

    matrix_utilities updated, added export to csv

--- local_freight_tool/matrix_utilities.py
+++ local_freight_tool/matrix_utilities.py
@@ -11,12 +11,10 @@ missing zones, removing external-external trips, and converting to UFM.
 
 """
 
-# User-defined imports
-from os import stat
-import zone_correspondence as zcorr
+# TODO add convert to UFM
+# TODO add rezoning
+# TODO add main function to call?
 
-# Other packages
-import geopandas as gpd
 import pandas as pd
 
 class ODMatrix:
@@ -39,39 +37,190 @@ class ODMatrix:
             pivoted matrix, by default True
         """
         if not pivoted:
-            self.matrix = dataframe
-            self.make_pivot()
+            # create pivoted version of dataframe
+            self.matrix = dataframe.pivot_table(index='origin', columns='destination', values='trips', fill_value=0)
         else:
-            self.pivoted_matrix = dataframe
-            self.melt_pivot()
+            self.matrix = dataframe
         
     def __str__(self):
-        return f'{self.matrix}'       
-
-    def make_pivot(self):
-        """Updates self.pivoted_matrix so that it is the pivoted version of
-        self.matrix such that the row indices are origins, column indices are
-        destinations, and values are trips.
+        """Sets the column version of the OD matrix as the string output.
 
         Returns
         -------
-        self
-            The updated class instance
+        str
+            string representation of column matrix with columns 'origin',
+            'destination' and 'trips,
         """
-        self.pivoted_matrix = self.matrix.pivot_table(index='origin', columns='destination', values='trips', fill_value=0)
-        return self
+        return f'{self.column_matrix()}'
 
-    def melt_pivot(self):
-        """Updates self.matrix from self.pivoted_matrix, where self.matrix
-        has 'origin', 'destination' and 'trips' as columns.
+    def __repr__(self):
+        """Sets the pivoted matrix as the representation of the OD matrix.
 
         Returns
         -------
-        self
-            The updated class instance
+        str
+            string representation of pivoted O-D matrix.
         """
-        self.matrix = self.pivoted_matrix.melt(value_name='trips', ignore_index=False).reset_index().sort_values(by='origin', ignore_index=True)
+        return f'{self.matrix}'
+    
+    def __add__(self, other_matrix):
+        """Add two ODMatrix objects, element-wise. This first aligns the
+        matrices then sums them.
+
+        Parameters
+        ----------
+        self : ODMatrix.Object
+            First matrix instance
+        other_matrix : ODMatrix.Object
+            Second matrix instance
+
+        Returns
+        -------
+        ODMatrix.Object
+            Sum of input matrices
+        """
+        matrix_1_aligned, matrix_2_aligned = self.align(self, other_matrix)
+        sum = matrix_1_aligned + matrix_2_aligned
+
+        return ODMatrix(sum)
+    
+    def __sub__(self, other_matrix):
+        """Subract a matrix from the current matrix instance, element-wise. This first aligns the
+        matrices then subtracts.
+
+        Parameters
+        ----------
+        self : ODMatrix.Object
+            Matrix instance to subtract from
+        other_matrix : ODMatrix.Object
+            Matrix to subtract
+
+        Returns
+        -------
+        ODMatrix.Object
+            Matrix with other_matrix subtracted
+        """
+        matrix_1_aligned, matrix_2_aligned = self.align(self, other_matrix)
+        subtracted = matrix_1_aligned - matrix_2_aligned
+
+        return ODMatrix(subtracted)
+    
+    def __mul__(self, factor):
+        if (type(factor) == int) | (type(factor) == float):
+            factored = self.matrix * factor
+        elif type(factor) == ODMatrix:
+            matrix_1_aligned, matrix_2_aligned = ODMatrix.align(self, factor)
+            factored = matrix_1_aligned*matrix_2_aligned
+        else:
+            raise TypeError('Can only multiply an O-D matrix by a scalar or another O-D matrix')
+        
+        return ODMatrix(factored)
+    
+    def column_matrix(self, include_zeros=True):
+        """Transforms the matrix of the ODMatrix instance into a 3 column 
+        matrix, ideal for writing the output to a file.
+
+        Returns
+        -------
+        pd.DataFrame
+            3-column dataframe representing an O-D matrix with columns 
+            'origin', 'destination' and 'trips'.
+        """
+        column_matrix = self.matrix.melt(value_name='trips', ignore_index=False).reset_index().sort_values(by='origin', ignore_index=True)
+        if not include_zeros:
+            column_matrix = column_matrix[column_matrix.trips != 0]
+        return column_matrix
+
+    
+    def scalar_factor(self, factor):
+        """Apply a scalar factor to every element of a matrix.
+        This does not update the matrix itself, but creates a new ODMatrix
+        instance.
+
+        Parameters
+        ----------
+        factor : float
+            Factor to multiply each element of the matrix by.
+
+        Returns
+        -------
+        ODMatrix.Object
+            New factored instance of the ODMatrix Class
+        """
+        factored = self.matrix * factor
+        factored_od_matrix = ODMatrix(factored)
+        return factored_od_matrix
+
+    def fill_missing_zones(self, missing_zones):
+        """Updates OD matrix to include missing zones. Missing zone values are
+        set to 0.
+
+        Parameters
+        ----------
+        missing_zones : list or pd.DataFrame
+            list of missing zones or 1 column DataFrame of missing zones
+
+        Returns
+        -------
+        ODMatrix
+            Updated ODMatrix with missing zones included
+        """
+        # check if zones given as list or DataFrame
+        if type(missing_zones) == pd.DataFrame:
+            missing_zones = list(missing_zones[0])
+        
+        # check that the zones are missing, if there are any that appear in
+        # the matrix, remove them from the missing zones list
+        shared_zones = self.matrix[self.matrix.index.isin(missing_zones)]
+        if len(shared_zones) > 0:
+            missing_zones = missing_zones.remove(shared_zones.index)
+
+        # add 0 value columns and rows to matrix 
+        columns_to_add = pd.DataFrame(0, index=self.matrix.index, columns=missing_zones)
+        self.matrix = pd.concat([self.matrix, columns_to_add], axis=1, join='outer')
+        rows_to_add = pd.DataFrame(0, index=missing_zones, columns=self.matrix.columns)
+        self.matrix = pd.concat([self.matrix, rows_to_add], axis=0, join='outer')
+
         return self
+
+    def remove_external_trips(self, external_zones):
+        """Sets external-external trips to 0. Returns a new O-D matrix.
+
+        Parameters
+        ----------
+        external_zones : list or pd.DataFrane
+            List or 1 column DataFrame of external zones
+
+        Returns
+        -------
+        ODMatrix
+            New ODMatrix with the external-external trips set to 0.
+        """
+        # check if zones given as list or DataFrame
+        if type(external_zones) == pd.DataFrame:
+            external_zones = list(external_zones[0])
+        
+        # only set trips to 0 for zones that are already in the matrix
+        shared_zones = self.matrix[self.matrix.index.isin(external_zones)]
+        external_zones = list(shared_zones.index)
+
+        # set external-external trip values to 0
+        ee_trips_removed = self.matrix.loc[external_zones, external_zones]
+
+        return ODMatrix(ee_trips_removed)
+
+    def export_to_csv(self, filepath, include_zeros=True):
+        """Export column matrix to csv file
+
+        Parameters
+        ----------
+        filepath : str
+            Path to save output file to
+        include_zeros : bool, optional
+            Whether to include zero-value trips, by default True
+        """
+        column_matrix = self.column_matrix(include_zeros=include_zeros)
+        column_matrix.to_csv(filepath, index=False)
 
     @classmethod
     def read_OD_file(cls, filepath):
@@ -93,67 +242,6 @@ class ODMatrix:
         matrix_dataframe = pd.read_csv(filepath, delim_whitespace=whitespace, header=header_row, names=['origin', 'destination', 'trips'], usecols=[0, 1, 2])
         matrix = cls(matrix_dataframe, pivoted=False)
         return matrix
-    
-    def scalar_factor(self, factor):
-        """Apply a scalar factor to every element of a matrix.
-        This does not update the matrix itself, but creates a new ODMatrix
-        instance.
-
-        Parameters
-        ----------
-        factor : float
-            Factor to multiply each element of the matrix by.
-
-        Returns
-        -------
-        ODMatrix.Object
-            New factored instance of the ODMatrix Class
-        """
-        factored = self.pivoted_matrix * factor
-        return self(factored)
-    
-    @classmethod
-    def matrix_addition(cls, matrix_1, matrix_2):
-        """Add two ODMatrix objects, element-wise. This first aligns the
-        matrices then sums them.
-
-        Parameters
-        ----------
-        matrix_1 : ODMatrix.Object
-            First matrix instance
-        matrix_2 : ODMatrix.Object
-            Second matrix instance
-
-        Returns
-        -------
-        ODMatrix.Object
-            Sum of input matrices
-        """
-        matrix_1_aligned, matrix_2_aligned = cls.align(matrix_1, matrix_2)
-        sum = matrix_1_aligned + matrix_2_aligned
-
-        return cls(sum)
-    
-    @classmethod
-    def matrix_factoring(cls, matrix_1, matrix_2):
-        """Factor two matrices element-wise.
-
-        Parameters
-        ----------
-        matrix_1 : ODMatrix.Object
-            First matrix instance
-        matrix_2 : ODMatrix.Object
-            Second matrix instance
-
-        Returns
-        -------
-        ODMatrix.Object
-            Factored matrix instance
-        """
-        matrix_1_aligned, matrix_2_aligned = cls.align(matrix_1, matrix_2)
-        factored = matrix_1_aligned*matrix_2_aligned
-
-        return cls(factored)
 
     @staticmethod
     def align(matrix_1, matrix_2):
@@ -174,7 +262,7 @@ class ODMatrix:
         pd.DataFrame
             Aligned version of matrix_2's pivoted DataFrame
         """
-        return matrix_1.pivoted_matrix.align(matrix_2.pivoted_matrix, join='outer', fill_value = 0)
+        return matrix_1.matrix.align(matrix_2.matrix, join='outer', fill_value = 0)
 
     @staticmethod
     def check_file_header(filepath):
@@ -214,11 +302,3 @@ class ODMatrix:
             header_row = 0
 
         return whitespace, header_row
-
-
-if __name__ == "__main__":
-    od1path = 'C:/WSP_projects/Freight/matrix_utilities_files/od_matrices/od_matrix_1.csv'
-    od2path = 'C:/WSP_projects/Freight/matrix_utilities_files/od_matrices/od_matrix_2.csv'
-    odmatrix = ODMatrix.read_OD_file(odpath)
-    print(odmatrix.matrix)
-    print(odmatrix.pivoted_matrix)
