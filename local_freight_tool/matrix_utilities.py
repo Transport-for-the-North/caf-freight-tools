005418bc458cd33304f296d06455188274f4e4ec
clynch-wsp <cara.lynch@wsp.com>
2021-03-05 16:18:12 +0000

    changed matrix summary

--- local_freight_tool/matrix_utilities.py
+++ local_freight_tool/matrix_utilities.py
@@ -170,13 +170,12 @@ class ODMatrix:
         summary = {
             "Name": self.name,
             "Total": column_matrix.trips.sum(),
-            "Count": len(column_matrix.trips),
+            "Cell Count": len(column_matrix.trips),
             "Mean": column_matrix.trips.mean(),
             "Standard deviation": column_matrix.trips.std(),
             "Max": max(column_matrix.trips),
             "Min": min(column_matrix.trips),
             "0 count": (column_matrix.trips == 0).sum(),
-            "NaN count": column_matrix.trips.isna().sum(),
         }
 
         return summary
