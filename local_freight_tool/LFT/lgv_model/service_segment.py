79c31da9b6320322869388bf55ef691dd87de34e
Matt <matthew.buckley@wsp.com>
2021-06-18 15:25:45 +0100

    Added calculations for service trip ends
    Added additional properties to `ServiceTripEnds` to calculate the trip
    ends and itemediary steps.

--- local_freight_tool/LFT/lgv_model/service_segment.py
+++ local_freight_tool/LFT/lgv_model/service_segment.py
@@ -9,6 +9,7 @@
 from pathlib import Path
 
 # Third party imports
+import pandas as pd
 
 # Local imports
 from .. import utilities, errors
@@ -47,8 +48,8 @@ class ServiceTripEnds:
     """
 
     BRES_AGGREGATION = {
-        "office": list(lgv_inputs.letters_range("I", "P")),
-        "other": list(lgv_inputs.letters_range(end="H"))
+        "Office": list(lgv_inputs.letters_range("I", "P")),
+        "Other": list(lgv_inputs.letters_range(end="H"))
         + list(lgv_inputs.letters_range("Q", "U")),
     }
     SERVICE_TRIPS_HEADER = {"Segment": str, "Annual Service Trips": int}
@@ -62,6 +63,31 @@ class ServiceTripEnds:
         scale_factor: float,
     ):
         """Initilise class by checking input files exist and are expected type."""
+        # Check all given parameters
+        self._check_paths(household_paths, bres_paths, service_trips)
+        try:
+            self._scale_factor = float(scale_factor)
+        except ValueError as e:
+            raise ValueError(
+                f"scale_factor should be a numeric type not '{type(scale_factor)}'"
+            ) from e
+        if self._scale_factor <= 0:
+            raise ValueError(f"scale_factor should be >= 0 not {self._scale_factor}")
+        # Initlise instance variables defined later
+        self.households = None
+        self.bres = None
+        self.total_trips = None
+        self._trip_proportions = None
+        self._trips = None
+        self._trip_ends = None
+
+    def _check_paths(
+        self,
+        household_paths: tuple[Path, Path],
+        bres_paths: tuple[Path, Path],
+        service_trips: Path,
+    ):
+        """Checks the input files exist and are the expected type."""
         self._household_path = utilities.check_file_path(
             household_paths[0], "Household data", ".csv", ".txt", return_path=True
         )
@@ -77,17 +103,6 @@ class ServiceTripEnds:
         self._trips_path = utilities.check_file_path(
             service_trips, "Service trips", ".csv", ".txt", return_path=True
         )
-        try:
-            self._scale_factor = float(scale_factor)
-        except ValueError as e:
-            raise ValueError(
-                f"scale_factor should be a numeric type not '{type(scale_factor)}'"
-            ) from e
-        if self._scale_factor <= 0:
-            raise ValueError(f"scale_factor should be >= 0 not {self._scale_factor}")
-        self.households = None
-        self.bres = None
-        self.total_trips = None
 
     def read(self):
         """Read the input data and perform any necessary conversions.
@@ -107,9 +122,11 @@ class ServiceTripEnds:
         self.households = lgv_inputs.household_projections(
             self._household_path, self._household_zc
         )
+        self.households.set_index("Zone", inplace=True)
         self.bres = lgv_inputs.filtered_bres(
             self._bres_path, self._bres_zc, self.BRES_AGGREGATION
         )
+        self.bres.set_index("Zone", inplace=True)
         # Read and check service trips data
         self.total_trips = utilities.read_csv(
             self._trips_path,
@@ -126,6 +143,60 @@ class ServiceTripEnds:
         self.total_trips = self.total_trips.loc[self.SERVICE_TRIPS_SEGMENTS, :].copy()
         # Factor service trips to model year
         self.total_trips *= self._scale_factor
+
+    @property
+    def trip_proportions(self) -> pd.DataFrame:
+        """pd.DataFrame : Proportion of input data for each zone, with
+        zone number as index (Zone) and columns Households, Office
+        (employees) and Other (employees). Each column sums to 1.
+        """
+        if self._trip_proportions is None:
+            if self.households is None or self.bres is None:
+                raise ValueError(
+                    "cannot calculate trip proportions until input data "
+                    "has been read, call `ServiceTripEnds.read` first"
+                )
+            trip_data = pd.concat([self.households, self.bres], axis=1)
+            self._trip_proportions = trip_data / trip_data.sum(axis=0)
+        return self._trip_proportions
+
+    @property
+    def trips(self) -> pd.DataFrame:
+        """pd.DataFrame : Number of trips for each zone (index) and
+        each of the segments (columns) Residential, Office and Other.
+        """
+        if self._trips is None:
+            if self.total_trips is None:
+                raise ValueError(
+                    "cannot calculate trips until input data has "
+                    "been read, call `ServiceTripEnds.read` first"
+                )
+            self._trips = self.trip_proportions.copy()
+            self._trips.rename(columns={"Households": "Residential"}, inplace=True)
+            self._trips["Residential"] *= self.total_trips.at[
+                "Residential", "Annual Service Trips"
+            ]
+            self._trips["Office"] *= self.total_trips.at[
+                "Office", "Annual Service Trips"
+            ]
+            self._trips["Other"] *= self.total_trips.at[
+                "All Other", "Annual Service Trips"
+            ]
+        return self._trips
+
+    @property
+    def trip_ends(self) -> pd.DataFrame:
+        """pd.DataFrame : Productions and Attractions (columns)
+        trip ends for all zones (index).
+        """
+        if self._trip_ends is None:
+            # Aggregate trips together
+            tot_trips = self.trips.sum(axis=1)
+            # Divide trips by 2 to get productions and attractions (both are identical)
+            self._trip_ends = pd.DataFrame(
+                {"Productions": tot_trips / 2, "Attractions": tot_trips / 2}
+            )
+        return self._trip_ends
 
 
 # TODO Remove test code
@@ -149,4 +220,12 @@ if __name__ == "__main__":
         (hh_path, zc_path), (bres_path, bres_zc_path), service_path, 1.51
     )
     service_te.read()
-    print(service_te.households, service_te.bres, service_te.total_trips, sep="\n")
+    print(
+        service_te.households,
+        service_te.bres,
+        service_te.total_trips,
+        service_te.trip_proportions,
+        service_te.trips,
+        service_te.trip_ends,
+        sep="\n",
+    )
