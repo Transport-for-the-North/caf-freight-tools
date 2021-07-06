cc14fc1817148c29b3e1d97ba2fee061e95a32ce
Matt <matthew.buckley@wsp.com>
2021-07-06 17:33:45 +0100

    Added gravity model and cost functions
    Implemented the gravity model, tanner and `log_normal` functions.
    Added a small test function to run `gravity_model_equation` with a small
    3x3 matrix and print the results.

---
+++ local_freight_tool/LFT/lgv_model/gravity_model.py
@@ -0,0 +1,252 @@
+# -*- coding: utf-8 -*-
+"""
+    Module containing the gravity model used in the LGV model.
+"""
+
+##### IMPORTS #####
+# Standard imports
+
+# Third party imports
+import numpy as np
+import pandas as pd
+
+# Local imports
+from .. import errors
+
+
+##### FUNCTIONS #####
+def _check_numeric(**kwargs) -> None:
+    """Checks if given parameters are floats or ints.
+
+    Raises
+    ------
+    ValueError
+        If any of the parameters aren't floats or ints,
+        includes the parameter name in the message.
+    """
+    for nm, val in kwargs.items():
+        if not isinstance(val, (int, float)):
+            raise ValueError(
+                f"{nm} should be a scalar number (float or int) not {type(val)}"
+            )
+
+
+def tanner(cost: np.ndarray, alpha: float, beta: float) -> np.ndarray:
+    r"""Implemenation of the travel cost tanner function.
+
+    Parameters
+    ----------
+    cost : np.ndarray
+        Array of the travel costs.
+    alpha, beta : float
+        Parameter for the tanner formula, see Notes.
+
+    Returns
+    -------
+    np.ndarray
+        Output from the tanner equation,
+        same shape as `cost`.
+
+    Notes
+    -----
+    Formula used for this function is:
+
+    .. math:: f(C_{ij}) = C_{ij}^\alpha \cdot \exp(\beta C_{ij})
+
+    where:
+
+    - :math:`C_{ij}`: cost from i to k.
+    - :math:`\alpha, \beta`: calibration parameters.
+    """
+    _check_numeric(alpha=alpha, beta=beta)
+    power = np.float_power(cost, alpha)
+    exp = np.exp(beta * cost)
+    return power * exp
+
+
+def log_normal(cost: np.ndarray, sigma: float, mu: float) -> np.ndarray:
+    r"""Implementation of the travel cost log normal function.
+
+    Parameters
+    ----------
+    cost : np.ndarray
+        Array of the travel costs.
+    sigma, mu : float
+        Parameter for the equation, see Notes.
+
+    Returns
+    -------
+    np.ndarray
+        Output from the log normal equation,
+        same shape as `cost`.
+
+    Notes
+    -----
+    Formula used for this function is:
+
+    .. math::
+
+        f(C_{ij}) = \frac{1}{C_{ij} \cdot \sigma \cdot \sqrt{2\pi}}
+        \cdot \exp\left(-\frac{(\ln C_{ij}-\mu)^2}{2\sigma^2}\right)
+
+    where:
+
+    - :math:`C_{ij}`: cost from i to j.
+    - :math:`\sigma, \mu`: calibration parameters.
+    """
+    _check_numeric(sigma=sigma, mu=mu)
+    frac = 1 / (cost * sigma * np.sqrt(2 * np.PI))
+    exp_numerator = (np.ln(cost) - mu) ** 2
+    exp_denominator = 2 * sigma ** 2
+    exp = np.exp(-exp_numerator / exp_denominator)
+    return frac * exp
+
+
+def _check_gm_inputs(
+    trip_ends: pd.DataFrame, costs: pd.DataFrame, calibration: pd.DataFrame
+) -> None:
+    """Sorts the indices and checks the input DataFrames for `gravity_model_equation`."""
+    args = {"trip_ends": trip_ends, "costs": costs, "calibration": calibration}
+    for nm, df in args.items():
+        df.sort_index(axis=0, inplace=True)
+        if df.index.has_duplicates:
+            raise ValueError(f"duplicates not allowed in `{nm}` index")
+        if df.columns.has_duplicates:
+            raise ValueError(f"duplicates not allowed in `{nm}` columns")
+        if nm == "trip_ends":
+            continue
+        df.sort_index(axis=1, inplace=True)
+        same = df.index.equals(trip_ends.index) and df.columns.equals(trip_ends.index)
+        if not same:
+            raise ValueError(
+                f"`{nm}` must be a square matrix with same zones as "
+                "`trip_ends` for gravity model calculations"
+            )
+
+
+def gravity_model_equation(
+    trip_ends: pd.DataFrame,
+    costs: pd.DataFrame,
+    calibration: pd.DataFrame = None,
+    normalise_costs: bool = True,
+    function: str = "tanner",
+    **kwargs,
+) -> pd.DataFrame:
+    r"""Implementation of the main Gravity Model function, see Notes.
+
+    Parameters
+    ----------
+    trip_ends : pd.DataFrame
+        Dataframe containing columns Attractions and
+        Productions, where the index is the zone number.
+    costs : pd.DataFrame
+        Square matrix containing the costs between all
+        zones, index and columns should both be equal
+        to the `trip_ends` index.
+    calibration : pd.DataFrame, optional
+        Matrix of calibration parameters, should be
+        the same shape as costs if given.
+    normalise_costs : bool, default True
+        If costs should be normalised or not.
+    function : {'tanner', 'log_normal'}
+        Choice of cost function to use.
+
+    Returns
+    -------
+    pd.DataFrame
+        DataFrame containing trips, same shape as costs.
+
+    Raises
+    ------
+    KeyError
+        If a `function` is given which doesn't exist.
+    ValueError
+        If any of the input DataFrames have duplicates in the
+        index or they don't have the same index and columns as
+        the `trip_ends` index.
+
+    Notes
+    -----
+    Formula use for this function is:
+
+    .. math::
+
+        T_{ij} = T_i \frac{A_j f(C_{ij}) K_{ij}}
+        {\sum^n_{j'=1} A_{j'} f(C_{ij'}) K_{ij'}}
+
+    where:
+
+    - :math:`T_{ij}`: trips from i to j.
+    - :math:`T_i`: trips from i (productions).
+    - :math:`A_j`: trips attracted to j.
+    - :math:`f(C_{ij})`: travel cost friction factor (see below).
+    - :math:`K_{ij}`: calibration parameter.
+
+    See Also
+    --------
+    tanner, log_normal
+    """
+    FUNCTION_LOOKUP = {
+        "log_normal": log_normal,
+        "tanner": tanner,
+    }
+    try:
+        func = FUNCTION_LOOKUP[function.lower().strip()]
+    except KeyError as e:
+        raise KeyError(
+            f"unknown function {function!r}, should be "
+            f"one of {list(FUNCTION_LOOKUP.keys())}"
+        ) from e
+    # Check trip_ends has the correct names
+    trip_ends.rename(columns=lambda nm: nm.strip().lower(), inplace=True)
+    missing = [c for c in ("attractions", "productions") if c not in trip_ends.columns]
+    if missing:
+        errors.MissingColumnsError("gravity model trip ends", missing)
+    # Check costs and calibrations are matrices with same zones as trip_ends
+    if calibration is None:
+        calibration = pd.DataFrame(1.0, index=trip_ends.index, columns=trip_ends.index)
+    _check_gm_inputs(trip_ends, costs, calibration)
+
+    # Convert vectors of productions and attractions to matrices for calcs
+    matrices = [
+        np.tile(trip_ends.productions, (len(trip_ends.productions), 1)).T,
+        np.tile(trip_ends.attractions, (len(trip_ends.attractions), 1)),
+    ]
+    # TODO Check if the costs should be normalised or not?
+    if normalise_costs:
+        costs = costs / np.sum(costs.values)
+    numerator = pd.DataFrame(
+        matrices[0] * matrices[1] * func(costs.values, **kwargs) * calibration.values,
+        index=trip_ends.index,
+        columns=trip_ends.index,
+    )
+    # Denominator uses the total attractions, and then the sum of
+    # all the columns (j) for costs and calibration factors
+    denominator = (
+        np.sum(trip_ends.attractions.values)
+        * func(np.sum(costs.values, axis=1), **kwargs)
+        * np.sum(calibration.values, axis=1)
+    )
+    # Divide all the columns of the numerator by the 1D denominator array
+    return numerator.divide(denominator, axis=0)
+
+
+def main():
+    pass
+
+
+def test_gm_func():
+    """Runs the `gravity_model_equation` with small 3x3 matrix."""
+    rng = np.random.default_rng(1)
+    zones = [1, 2, 3]
+    trip_ends = pd.DataFrame(
+        {"attractions": [105, 115, 125], "productions": [110, 120, 130]}, index=zones
+    )
+    costs = pd.DataFrame(rng.integers(1000, 2000, (3, 3)), index=zones, columns=zones)
+    trips = gravity_model_equation(trip_ends, costs, function="tanner", alpha=1, beta=1)
+    print("Trip Ends:", trip_ends, "Costs:", costs, "Trips:", trips, sep="\n")
+
+
+##### MAIN #####
+if __name__ == "__main__":
+    test_gm_func()
